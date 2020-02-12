27632124002da0bab0618d88177578d84897ab60
kong <kmlor031@gmail.com>
2020-02-12 15:22:54 -0600

    Code base refactoring, and Command handling

---
+++ src/com/generic/util/Commands.java
@@ -0,0 +1,90 @@
+package com.generic.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.parser.ParseException;
+
+import com.generic.tracker.WarehouseTracker;
+
+public class Commands {
+	private static Commands command;
+
+	private static Map<String, Executable> commandList;
+	
+	private Commands() {}
+	
+	public void execute(String command, String[] args) {
+		commandList.get(command).execute(args);
+	}
+	
+	public static Commands getInstance() {
+		if (commandList == null) {
+			synchronized(Commands.class) {
+				command = new Commands();
+				commandList = new HashMap<>();
+				init();
+			}
+		}
+		return command;
+	}
+	
+	public static List<String> getCommandList() {
+		return new ArrayList<>(commandList.keySet());
+	}
+	
+	public static boolean exist(String command) {
+		return commandList.containsKey(command);
+	}
+	
+	private static void init() {
+		// some annotations here to help users how to use commands
+		commandList.put("import", (arg -> {
+			try {
+				System.out.println(arg);
+				new Persistent().parseJson(new File("resource/" + arg[0]).getAbsolutePath());
+			} catch (IOException | ParseException e) {
+				System.out.println("** System can not read the file!");
+				e.printStackTrace();
+			}
+		}));
+		
+		commandList.put("export", (arg -> {
+				WarehouseTracker warehouseTracker = WarehouseTracker.getInstance();
+				try {
+					int mWarehouseID = Integer.parseInt(arg[0]);
+					if (!warehouseTracker.warehouseExists(mWarehouseID)) {
+						return;
+					}
+					warehouseTracker.exportWarehouseToJSON(mWarehouseID);
+					System.out.println("Sucessfully exported warehouse");
+				} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+					System.out.println("** Please input a warehouse ID to export");
+				}
+		}));
+		
+		commandList.put("print", (arg -> {
+				int warehouseID;
+				try {
+					WarehouseTracker warehouseTracker = WarehouseTracker.getInstance();
+					warehouseID = Integer.parseInt(arg[1]);
+					warehouseTracker.printWarehouseDetails(warehouseID);
+				} catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+					System.out.println("** Please input a warehouseID");
+				}
+		}));
+		
+		commandList.put("printall", (arg -> {
+			WarehouseTracker warehouseTracker = WarehouseTracker.getInstance();
+			if (!warehouseTracker.isEmpty()) {
+					warehouseTracker.printAll();
+				} else {
+					System.out.println("** No warehouses avaliable, please import data first");
+				}
+		}));
+	}
+}
---
+++ src/com/generic/util/Executable.java
@@ -0,0 +1,5 @@
+package com.generic.util;
+
+public interface Executable {
+	public void execute(String[] args);
+}
