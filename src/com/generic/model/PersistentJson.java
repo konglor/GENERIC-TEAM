66c4c64a21ffa36939036b3ab7e515f53dc9ea0c
kong <kmlor031@gmail.com>
2020-02-12 20:03:03 -0600

    PersistentJson interface

--- src/com/generic/model/PersistentJson.java
+++ src/com/generic/model/PersistentJson.java
@@ -2,7 +2,7 @@ package com.generic.model;
 
 import org.json.simple.JSONObject;
 
-public abstract class PersistentJson {
+public abstract class PersistentJson implements IPersistentJson {
 	
 	protected String id;
 	
